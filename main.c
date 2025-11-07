cbdf35fa73ad2027fe6ca2a7b668843318894e02
woddy <gabrielcressoni@gmail.com>
2025-11-07 15:46:39 -0300

    pong

--- main.c
+++ main.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    load_rom(&cpu, "tetris.ch8");
+    load_rom(&cpu, "pong.ch8");
 
     // main loop
     while (cpu.state != QUIT) {
